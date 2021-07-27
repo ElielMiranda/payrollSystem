b607c909bdab07f0e315736539ac10beae975e7c
ElielMiranda <elielmiranda3@gmail.com>
2021-07-27 15:39:01 -0300

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,12 @@
+
+public abstract class Employee {
+	private static int id;
+	private String name;
+	private String address;
+	private int tipo; //Os tipos serão 1-hourly, 2-salaried, 3-commissioned
+	private TimeCard timeCard;
+	
+	
+}
+
+
